0d2dba03f7a2abe3197a2c98244cb72b1a9135c7
Kevin Lowe <k.james.lowe@gmail.com>
2014-06-15 00:10:37 -0700

    Cleaned up support functions
    there is now a dedicated function for centring text, using the
    camelCase naming convention for functions, new distance and compass to
    string functions.

--- reverseGeocache/reverseGeocache.ino
+++ reverseGeocache/reverseGeocache.ino
@@ -1,3 +1,26 @@
+/*
+
+  reverseGeocache.ino - Software for a box that only opens one place on earth. 
+  
+  Created: August 23, 2011
+  Author: Kevin Lowe
+  
+  This file is based on code from Arduiniana written by Mikal Hart.
+  "Reverse Geocache" is a trademark of Mikal Hart.
+  
+  Decription: This code is written for a reverse geocaching box. The box is 
+  activated by pressing a button. If the box is in the correct location it will
+  unlock. If the user passes a certain number of attempts, the box will never open.
+
+  Written for this hardware:
+  - Arduino UNO
+  - Modified Sparkfun GPS Shield with Pololu Switch and headers
+  - EM-406A SiRF III GPS Receiver
+  - LK202-25-WB - 2x20 LCD from Matrix Orbital
+  - Parallax Servo
+  
+*/
+
 #include <PWMServo.h>
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
@@ -18,6 +41,7 @@ static const int MAX_ATTEMPTS = 50;
 static const int HINT_ON_ATTEMPT = 46;
 static const int EEPROM_POSITION_ATTEMPTS = 14;
 static const int EEPROM_POSITION_SOLVED = 15;
+static const int LCD_CHARS_PER_LINE = 20;
 
 /** Objects */
 TinyGPS tinyGPS;
@@ -53,15 +77,15 @@ void setup()
   // Setup Off Pin
   pinMode(offPin, OUTPUT);
   digitalWrite(offPin, LOW);
-
+  
   // Are we already solved?
   if(EEPROM.read(EEPROM_POSITION_SOLVED) == 1)
   {
-    Msg("  Already solved!!", "", 2000);
+    writeToLCD("  Already solved!!", "", 2000);
     delay(1000);
     servo.write(OPEN_ANGLE);
     delay(3000);   
-    PowerOff();
+    powerOff();
   }
 
   // How many attempts have we done?
@@ -75,13 +99,13 @@ void setup()
   // Turn off if we've used all our attempts.
   if (attemptCounter > MAX_ATTEMPTS)
   {
-    Msg(" I'm sealed forever", "", 2000);
-    PowerOff();
+    writeToLCD(" I'm sealed forever", "", 2000);
+    powerOff();
   }
   
   // Introduction
-  Msg("There is only", "1 place on earth...", 3000);
-  Msg("..where I will open", "", 3000);
+  writeToLCD("There is only", "1 place on earth...", 3000);
+  writeToLCD("..where I will open", "", 3000);
   clearLCD();
   delay(1500);
 
@@ -100,7 +124,7 @@ void setup()
   lcd.print(attemptsLeft);
   delay(2000);   
   
-  Msg("    Finding your", "      location", 1000);
+  writeToLCD("    Finding your", "      location", 1000);
 }
 
 
@@ -126,7 +150,7 @@ void loop()
       // NEARBY: Open up, the puzzle is solved!
       if (distance_meters <= RADIUS)
       {
-        Msg("     Welcome to", "  Downtown Kigali", 2000);
+        writeToLCD("     Welcome to", "  Downtown Kigali", 2000);
         EEPROM.write(EEPROM_POSITION_SOLVED, true);
         delay(1000);
         servo.write(OPEN_ANGLE);
@@ -136,95 +160,104 @@ void loop()
       // FAR AWAY: Don't open. Direction and distance.
       else
       {
-        Msg("   Access Denied!", "", 2000);
-        Msg("", "", 1000);
+        writeToLCD("   Access Denied!", "", 2000);
+        writeToLCD("", "", 1000);
         
         // Turn off if we've used all our attempts.
         if (attemptCounter == MAX_ATTEMPTS)
         {
-          Msg(" I'm sealed forever", "", 2000);
-          PowerOff();
+          writeToLCD(" I'm sealed forever", "", 2000);
+          powerOff();
         }        
         
         // Calculate Direction and display on top line
         float courseto = TinyGPS::course_to(lat, lon, DEST_LATITUDE, DEST_LONGITUDE);
+
         clearLCD();
-        if (courseto >= 22.5 && courseto < 67.5)          lcd.print("  Travel Northeast");
-        else if (courseto >= 67.5 && courseto < 112.5)    lcd.print("    Travel East");
-        else if (courseto >= 112.5 && courseto < 157.5)   lcd.print("  Travel Southeast");
-        else if (courseto >= 157.5 && courseto < 202.5)   lcd.print("    Travel South");
-        else if (courseto >= 202.5 && courseto < 247.5)   lcd.print("  Travel Southwest");
-        else if (courseto >= 247.5 && courseto < 292.5)   lcd.print("    Travel West");
-        else if (courseto >= 292.5 && courseto < 337.5)   lcd.print("  Travel Northwest");
-        else if (courseto >= 337.5 || courseto < 22.5)    lcd.print("    Travel North");
-        else lcd.print("???");
+        lcd.print(centerText("Travel " + degreesToCompassPoint(courseto)));
         lcd.println();
 
         // Display distance on bottom line
-        DisplayDistanceString(distance_meters);
+        lcd.print(centerText("by " + distanceToString(distance_meters)));
         
         // Display the major hint within the last few attempts.
         if(attemptCounter >= HINT_ON_ATTEMPT)
         {
           delay(2000);
           clearLCD();  
-          Msg("       To The", " Kigali Roundabout", 0);
+          writeToLCD("       To The", " Kigali Roundabout", 0);
         }
         
         delay(4000);
       }
 
-      PowerOff();
+      powerOff();
     }
   }
 
   /* Turn off after 4 minutes */
   if (millis() >= 180000)
   {
-    Msg("   No GPS signal", "Face lid up, outside", 60000);
-    PowerOff();
+    writeToLCD("   No GPS signal", "Face lid up, outside", 60000);
+    powerOff();
   }
 }
 
 /** Called to shut off the system using the Pololu switch */
-void PowerOff()
+void powerOff()
 {
   digitalWrite(offPin, HIGH);
   delay(3000);
 }
 
-/** Displaces in the distance, centered on a 20 character LCD line. */
-void DisplayDistanceString(float distance_meters)
+/** Converts a distance into a meter or kilometer string */
+String distanceToString(float distance_meters)
 {
   if (distance_meters < 1000)
   {
-    for (int i = 0; i < 7 - (int)(log10(distance_meters)/2); ++i)
-      lcd.print(" ");
-  
-    lcd.print("by ");
-    lcd.print(distance_meters, 0);
-    lcd.print("m");
-  }
-  else if (distance_meters < 10000)
-  {
-    lcd.print("      by ");
-    lcd.print(distance_meters / 1000, 1);
-    lcd.print("km");
+    return (String)(int)distance_meters + "m"; 
   }
   else
   {
-    unsigned int distance_km = distance_meters / 1000;
-    for (int i = 0; i < 7 - (int)(log10(distance_km)/2); ++i)
-      lcd.print(" ");
-  
-    lcd.print("by ");
-    lcd.print(distance_km);
-    lcd.print("km");
+    byte decimals = 1;
+    char kmBuffer[10];
+    dtostrf(distance_meters / 1000, 0, decimals, kmBuffer);
+    return (String)kmBuffer + "km"; 
   }
 }
 
+/** Converts the degrees to the nearest cardinal/intercardinal point on a compass */
+String degreesToCompassPoint(float courseto)
+{
+  if      (courseto >= 22.5  && courseto < 67.5)    return "Northeast";
+  else if (courseto >= 67.5  && courseto < 112.5)   return "East";
+  else if (courseto >= 112.5 && courseto < 157.5)   return "Southeast";
+  else if (courseto >= 157.5 && courseto < 202.5)   return "South";
+  else if (courseto >= 202.5 && courseto < 247.5)   return "Southwest";
+  else if (courseto >= 247.5 && courseto < 292.5)   return "West";
+  else if (courseto >= 292.5 && courseto < 337.5)   return "Northwest";
+  else if (courseto >= 337.5 || courseto < 22.5)    return "North";
+  else                                              return "???";  
+}
+
+/** Adds spaces such that the provided string will be centered on the LCD */
+String centerText(String text)
+{
+  if (text.length() > LCD_CHARS_PER_LINE)
+    return text;
+  
+  int add_spaces = (LCD_CHARS_PER_LINE - text.length()) / 2;
+  
+  String centered_string;
+  for (int i = 0; i < add_spaces; ++i)
+    centered_string += " ";
+    
+  return (centered_string + text);
+}
+
+
 /** Prints a message to the screen and waits a delay */
-void Msg(const char *top, const char *bottom, unsigned long del)
+void writeToLCD(String top, String bottom, unsigned long del)
 {
   clearLCD();
   lcd.print(top);
